6403774e7d7365b51d15ca78ed6a92ff6edc796c
Alexander Türk <alx91@me.com>
2020-04-03 13:20:44 +0200

    Fix Api for newest SwiftUI

--- DynamicColors/ContentView.swift
+++ DynamicColors/ContentView.swift
@@ -12,11 +12,11 @@ struct SectionHeader : View {
 	var body: some View {
 		HStack {
 			Text("Light")
-				.color(Color(white: 0))
+				.foregroundColor(Color(white: 0))
 				.frame(minWidth: 0, maxWidth: .infinity)
 				.background(Color(white: 0.9))
 			Text("Dark")
-				.color(Color(white: 1))
+				.foregroundColor(Color(white: 1))
 				.frame(minWidth: 0, maxWidth: .infinity)
 				.background(Color(white: 0))
 		}
@@ -26,30 +26,30 @@ struct SectionHeader : View {
 struct ContentView: View {
 	private let colors: [UIColor] = [.systemBlue, .systemGray, .systemGreen, .systemIndigo, .systemOrange, .systemPink, .systemPurple, .systemRed, .systemTeal, .systemYellow]
 	private let grays: [UIColor] = [.systemGray, .systemGray2, .systemGray3, .systemGray4, .systemGray5, .systemGray6]
-	private let dynamic: [UIColor] = [.label, .secondaryLabel, .tertiaryLabel, .quaternaryLabel, .placeholderText, .separator, .opaqueSeparator, .link]
+	private let dynamic: [UIColor] = [ .separator, .opaqueSeparator, .link]
 	private let swiftUIOnly: [Color] = [.primary, .secondary, .accentColor]
 
 	var body: some View {
 		NavigationView {
 			List {
 				SectionHeader()
-				Section(header: Text("System Colors (UIKit+SwiftUI)")) {
-					ForEach(colors.identified(by: \.self)) { color in
-						ColorRow(.uiColor(color))
-					}
-				}
-				Section(header: Text("System Grays (UIKit)")) {
-					ForEach(grays.identified(by: \.self)) { color in
-						ColorRow(.uiColor(color))
-					}
-				}
+//				Section(header: Text("System Colors (UIKit+SwiftUI)")) {
+//					ForEach(colors.identified(by: \.self)) { color in
+//						ColorRow(.uiColor(color))
+//					}
+//				}
+//				Section(header: Text("System Grays (UIKit)")) {
+//					ForEach(grays.identified(by: \.self)) { color in
+//						ColorRow(.uiColor(color))
+//					}
+//				}
 				Section(header: Text("Dynamic Colors (UIKit)")) {
-					ForEach(dynamic.identified(by: \.self)) { color in
+					ForEach(dynamic, id: \.self) { color in
 						ColorRow(.uiColor(color))
 					}
 				}
 				Section(header: Text("Others (SwiftUI)")) {
-					ForEach(swiftUIOnly.identified(by: \.self)) { color in
+					ForEach(swiftUIOnly, id: \.self) { color in
 						ColorRow(.swiftUiColor(color))
 					}
 				}
